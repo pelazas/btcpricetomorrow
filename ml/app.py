206d1f76a2f14ed91d32216b8e4ff95e83c8a9b5
pelazas <carlospelazas@gmail.com>
2025-02-26 15:51:54 +0100

    github actions

--- ml/app.py
+++ ml/app.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify
 import requests
 import pandas as pd
 from datetime import datetime
-from flask_cors import CORS  # For handling CORS if needed
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app) 
 
 def get_bitcoin_data(days=365):
     url = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
@@ -16,11 +16,10 @@ def get_bitcoin_data(days=365):
     }
     try:
         response = requests.get(url, params=params)
-        response.raise_for_status()  # Raise exception for HTTP errors
+        response.raise_for_status()
         data = response.json()
         prices = data.get('prices', [])
         
-        # Format data with datetime and price
         formatted_data = [{
             'date': datetime.fromtimestamp(price[0]/1000).strftime('%Y-%m-%d'),
             'price': price[1]
