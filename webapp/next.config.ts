b7aa7b3c41754190965204592cbd02ac232d624b
pelazas <carlospelazas@gmail.com>
2025-12-13 08:46:45 +0100

    small fix for build

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Skip ESLint during build
-  },
+
 };
 
 export default nextConfig;
